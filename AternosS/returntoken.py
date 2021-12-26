378f8d8902006905dd00e80ba276cc314e66b24e
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-12-25 20:19:36 -0500

    Replaced Session with CloudScraper, able to receive Serverlist

---
+++ AternosS/returntoken.py
@@ -0,0 +1,33 @@
+import js2py #type:ignore
+import time
+
+def convertToken (garbage):
+    line1 = garbage.text.split('const COOKIE_PREFIX = "ATERNOS";')
+    line2 = line1[1].split("</script>")
+    line3 = line2[1].split("{window")
+    print(line3[1])
+    time.sleep(5)
+
+    func = '''
+    //Decoder by broc.seib
+    function atob(s) {
+    var e={},i,b=0,c,x,l=0,a,r='',w=String.fromCharCode,L=s.length;
+    var A="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    for(i=0;i<64;i++){e[A.charAt(i)]=i;}
+    for(x=0;x<L;x++){
+        c=e[s.charAt(x)];b=(b<<6)+c;l+=6;
+        while(l>=8){((a=(b>>>(l-=8))&0xff)||(x<(L-2)))&&(r+=w(a));}
+    }
+    return r;
+};
+    function headache() {
+        var dict = {}
+        dict ''' + line3[1][:-6] + '''
+        console.log(dict["AJAX_TOKEN"])
+        return dict["AJAX_TOKEN"]
+    }
+    
+    '''
+    evaluate = js2py.EvalJs({'python_sum':sum})
+    evaluate.execute(func)
+    return evaluate.headache()
--- AternosS/serverclass.py
+++ AternosS/serverclass.py
@@ -1,4 +1,5 @@
 from hashlib import md5
+import hashlib
 import os
 from blurple import ui #type: ignore
 import requests #type:ignore
@@ -6,21 +7,26 @@ import configparser
 import time
 import cloudscraper #type:ignore
 import random
-import lxml.html #type:ignore
-import re
-from atjparse import exec
+from returntoken import convertToken
+from bs4 import BeautifulSoup #type:ignore
 
 
 class CustomSession:
     def __init__ (self):
         self.ses = requests.session()
         self.sec = None
-    def randval ():
+        self.token = ""
+    def randval (self):
         s = ""
         for i in range(16):
             s += str(random.randint(0,9))
         return s
-    def get (self, customprint = print, link = "", parameters = {}, headers = {}, inputcookies = {}, isUpdateToken = False):
+    def fancyget (self, customprint = print, link = "", parameters = {}, headers = {}, inputcookies = {}, isUpdateToken = False):
+        headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+        if self.token == "":
+            isUpdateToken = True
+        else: 
+            parameters["TOKEN"] = self.token
         if self.sec == None:
             val1 = self.randval()
             val2 = self.randval()
@@ -29,26 +35,55 @@ class CustomSession:
             newval2 = val2 + ";path=" + link
             self.ses.cookies.set((cookiekey + val1), newval2)
             customprint("Created randval")
-        newses = requests.session()
+            parameters["SEC"] = self.sec
+        newses = cloudscraper.create_scraper()
         newses.cookies = self.ses.cookies
-        r = newses.get(link, params= parameters, headers= headers, cookies= inputcookies)
-
+        r = newses.get(link , params= parameters, headers= headers, cookies= inputcookies)
         cloudfail = 0
-        while 'const COOKIE_PREFIX = "ATERNOS";' not in r.text and cloudfail < 20:
-            time.sleep(1)
+        while '<title>Please Wait... | Cloudflare</title>' in r.text and cloudfail < 20:
+            time.sleep(5)
             customprint("CloudFlare Error, Attempt Number: ", str(cloudfail + 1))
             cloudfail += 1
             r = newses.get(link, params= parameters, headers= headers, cookies= inputcookies)
-        if 'const COOKIE_PREFIX = "ATERNOS";' not in r.text:
+        if '<title>Please Wait... | Cloudflare</title>' in r.text:
             customprint("Failed to load Aternos.org")
         else:
-            webhead = lxml.html.fromString(r.content).head.textcontent()
-            
-            return r
-
-
-    def post():
-        pass
+            self.ses = newses
+            isArrowFunction = True
+            while isArrowFunction:
+                try:
+                    if isUpdateToken:
+                        self.token = convertToken(r)
+                        isArrowFunction = False
+                except:
+                    r = newses.get(link , params= parameters, headers= headers, cookies= inputcookies)
+    def fancypost(self, customprint = print, link = "", data = {}, headers = {}, inputcookies = {}):
+        headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+        newses = cloudscraper.create_scraper()
+        newses.cookies = self.ses.cookies
+        try:
+            data["TOKEN"] = self.token
+        except:
+            customprint("No Token provided")
+            return
+        if self.sec == None:
+            customprint("No SEC, run CustomSessio().get()")
+            return
+        else: data["SEC"] = self.sec
+        for item in self.ses.cookies.get_dict():
+            if "_SEC_" in item:
+                inputcookies[item] = self.ses.cookies.get(item)
+        r = newses.post(link, data = data, headers= headers, cookies = inputcookies)
+        cloudfail = 0
+        while '<title>Please Wait... | Cloudflare</title>' in r.text and cloudfail < 20:
+            time.sleep(5)
+            customprint("CloudFlare Error, Attempt Number: ", str(cloudfail + 1))
+            cloudfail += 1
+            r = newses.post(link, data= data, headers= headers, cookies= inputcookies)
+        if '<title>Please Wait... | Cloudflare</title>' in r.text:
+            customprint("Failed to load Aternos.org")
+        else:
+            self.ses = newses
 
 class server:
     def __init__ (self):
@@ -78,20 +113,21 @@ class User:
         if type(username) != str or type(password) != str:
             self.printfunc("Invalid User and Password")
             return None
-        
 
-sess = cloudscraper.create_scraper()
-r = sess.get("https://aternos.org/go").content
-bruh = re.findall(r'\(\(\)(.*?)\)\(\);', lxml.html.fromstring(r).head.text_content())
-print(len(bruh))
-ctx = exec(bruh[0])
-print(r)
-print("\n\n")
-print(bruh)
-print(ctx)
-print(ctx.window["AJAX_TOKEN"])
-
-
-
-# test = User("bruh", "bruh")
-
+sesh = CustomSession()
+sesh.fancyget(link = "https://aternos.org/go", isUpdateToken = True)
+print(md5("spedtreequestion".encode("utf-8")).hexdigest().lower())
+sesh.fancypost(
+    link = 'https://aternos.org/panel/ajax/account/login.php?SEC=' + sesh.sec + '&TOKEN=' + sesh.token,
+    data = {
+        "user": "spedtreequestion",
+        "password": hashlib.md5("spedtreequestion".encode("utf8")).hexdigest().lower()
+    }
+    )
+print(sesh.ses.cookies)
+r = sesh.ses.get('https://aternos.org/servers/')
+html = r.text
+goodSoup = BeautifulSoup(html, features="html.parser")
+gooddivs = str(goodSoup.find_all("div", {"class": "server"}))
+divlist = gooddivs.split("<div class=")
+print(divlist)
